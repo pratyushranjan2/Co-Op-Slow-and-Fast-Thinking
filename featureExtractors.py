e83a01288e52ea5f55371ce03dc22042322ce89f
* <*pratyushranjan02@gmail.com*>
2022-10-16 00:59:11 +0530

    biased ghost | pacmans in team1 do not weigh ghosts for deciding next cell

--- featureExtractors.py
+++ featureExtractors.py
@@ -89,7 +89,11 @@ class SimpleExtractor(FeatureExtractor):
         next_x, next_y = int(x + dx), int(y + dy)
 
         # count the number of ghosts 1-step away
-        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+        pacmanIndex = pacmanInfo['agentIndex']
+        if state.data.biasedGhost and pacmanIndex in state.data.team1:
+            features["#-of-ghosts-1-step-away"] = 0
+        else:
+            features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
 
         # if there is no danger of ghosts then add the food feature
         if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
