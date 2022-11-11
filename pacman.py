1c97c1013bfcc190ffd27bb8391238731c80bb7e
* <*pratyushranjan02@gmail.com*>
2022-11-12 02:26:15 +0530

    experiments with proximity blocking agents

--- pacman.py
+++ pacman.py
@@ -933,7 +933,7 @@ if __name__ == '__main__':
     team1Total = []
     team2Total = []
 
-    save = False
+    save = True
     save_file = '' # set in readCommand()
     # save = False
     # if save:
